58cd2caedde752ce74b1e9de1b31ef5316b63dbd
ITkieltyk <kieltyk+gh@gmail.com>
2023-05-16 01:42:40 +0200

    Query is using data input

--- src/app/flight-table/flight-table.component.ts
+++ src/app/flight-table/flight-table.component.ts
@@ -24,14 +24,24 @@ export class FlightTableComponent implements OnInit {
     takeoffDateStart: '',
     takeoffDateEnd: '',
   };
+
+  queryDateStart: Date = new Date();
+  queryDateEnd: Date = new Date();
   filteredFlights: Flight[] = this.service2.flights;
   ngOnInit(): void {
     this.query = this.service.bookingQuery;
+    this.queryDateStart = new Date(this.query.takeoffDateStart.valueOf());
+    this.queryDateEnd = new Date(this.query.takeoffDateEnd.valueOf());
 
     this.filteredFlights = this.service2.flights.filter(
       (el) =>
         el.destination.city === this.query.destination &&
-        el.origin.city === this.query.origin
+        el.origin.city === this.query.origin &&
+        el.date >= this.queryDateStart &&
+        el.date <= this.queryDateEnd
+    );
+    this.filteredFlights = this.filteredFlights.sort(
+      (a: Flight, b: Flight) => a.date.getTime() - b.date.getTime()
     );
   }
 }
